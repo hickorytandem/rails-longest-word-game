d54292ceec3b23c9057217be258793974adacde2
Michael Carter <hickorytandem@gmail.com>
2021-06-30 21:13:37 +0900

    did a little prep work. path not function for new or score

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,13 @@
+class GamesController < ApplicationController
+
+  def new
+    # display random grid and score
+    # something like this ('a'..'z').to_a.sample(10)
+    # store in @letters and display in the view
+  end
+
+  def score
+    # form will be submitted (with POST)
+  end
+
+end
